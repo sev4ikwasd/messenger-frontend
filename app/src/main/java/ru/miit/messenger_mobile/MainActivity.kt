e38e9840455a4e5917db4405b42b7420843cb1e7
Vsevolod Bezaev <vvbezaev@gmail.com>
2025-03-10 22:12:02 +0300

    feat: Simple view for chat

--- app/src/main/java/ru/miit/messenger_mobile/MainActivity.kt
+++ app/src/main/java/ru/miit/messenger_mobile/MainActivity.kt
@@ -4,13 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ru.miit.messenger_mobile.ui.theme.MessengermobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +41,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MessengermobileTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ChatApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +51,81 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ChatApp(modifier: Modifier = Modifier) {
+    var messageText by remember { mutableStateOf(TextFieldValue("")) }
+    var messages by remember { mutableStateOf(listOf(
+        Message("Message 1", "user", "20:02"),
+        Message("Message 2", "other_user", "21:03")
+    )) }
+    val currentUser = "user"
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MessengermobileTheme {
-        Greeting("Android")
+    Column (modifier = modifier.fillMaxSize().padding(8.dp)) {
+        LazyColumn(
+            modifier = modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            contentPadding = PaddingValues(8.dp)
+        ) {
+            items(messages.size) { index ->
+                MessageItem(
+                    message = messages[index],
+                    isCurrentUser = messages[index].senderId == currentUser
+                )
+            }
+        }
+
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            BasicTextField(
+                value = messageText,
+                onValueChange = { messageText = it },
+                modifier = modifier
+                    .weight(1f)
+                    .background(Color.LightGray, CircleShape)
+                    .padding(12.dp)
+            )
+
+            Spacer(modifier = modifier.width(8.dp))
+
+            Button(onClick = {}) {
+                Text(text = "Send")
+            }
+        }
     }
 }
+
+@Composable
+fun MessageItem(message: Message, isCurrentUser: Boolean, modifier: Modifier = Modifier) {
+    val alignment = if (isCurrentUser) Alignment.End else Alignment.Start
+    val backgroundColor = if (isCurrentUser) MaterialTheme.colorScheme.primary else Color.Gray
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalAlignment = alignment
+    ) {
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            shadowElevation = 1.dp,
+            color = backgroundColor
+        ) {
+            Text(
+                text = "${message.message}\n${message.timestamp}",
+                fontSize = 16.sp,
+                color = Color.White,
+                modifier = modifier.padding(12.dp)
+            )
+        }
+    }
+}
+
+data class Message(
+    val message: String,
+    val senderId: String,
+    val timestamp: String
+)
